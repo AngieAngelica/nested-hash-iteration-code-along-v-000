e2ab05e2d0dd1df6c57438ce1ac3bfd6d4b2bd53
Angie Kassab <anasab12@gmail.com>
2018-07-22 00:02:02 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -27,7 +27,7 @@ def remove_strawberry(contacts)
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-  contact.each do |person, contact_details_hash|
+  contacts.each do |person, contact_details_hash|
     binding.pry
   end 
 
